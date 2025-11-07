7d43ccb481cfbbf0bb9445ae4687e9a4434f9ac1
JayRocketMaN <odidikasomto@gmail.com>
2025-11-07 22:18:59 +0100

    edit redis.js2

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,8 @@
 import { createClient } from 'redis'
 import { ENV } from "./config.js";
 
-const client = createClient({ url: process.env.REDIS_URL })
-
+export default client = createClient({ url: process.env.REDIS_URL })
 await client.connect()
+
+
 
