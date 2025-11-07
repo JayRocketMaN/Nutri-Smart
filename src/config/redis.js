8317c771114ae128535e59ff4c71072243ceae1e
JayRocketMaN <162900153+JayRocketMaN@users.noreply.github.com>
2025-11-07 22:29:06 +0100

    Refactor Redis client setup and connection logic
    Refactor Redis client configuration and connection.

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,20 +1,10 @@
 // src/config/redis.js
 import { createClient } from "redis";
 import { ENV } from "./config.js";
-import { Logger } from "./logger.js";
 
-const redisUrl = ENV.REDIS_INTERNAL_URL || ENV.REDIS_URL;
 
-//export const redisClient = createClient({ ENV.REDIS_URL });
-export const redisClient = createClient({ url: redisUrl });
 
-export async function connectRedis() {
-  try {
-    redisClient.on("error", (err) => Logger.error("Redis Error:", err));
-    await redisClient.connect();
-    Logger.info("Redis connected");
-  } catch (err) {
-    Logger.error("Redis connection failed:", err);
-    throw err;
-  }
-}
+export default client = createClient({ url: process.env.REDIS_URL })
+await client.connect()
+
+
