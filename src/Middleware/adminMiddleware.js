848c092c1a1bbdc3172f5f2d574d734435e26864
Folarex10 <timothy.olaleye18@gmail.com>
2025-11-08 19:06:52 +0000

    Middleware updated

--- src/Middleware/adminMiddleware.js
+++ src/Middleware/adminMiddleware.js
@@ -1,6 +1,6 @@
 // src/middleware/adminMiddleware.js
 
-import AppError from "../utils/appError";
+import AppError from "../Utils/AppError.js";
 export const adminMiddleware = (req,res,next) => {
   if (!req.user) return next(new AppError("Unauthorized", 401));
   if (req.user.role !== "admin") return next(new AppError("Admin access required", 403));
