ced4d0cb4ce483a8a0014d46d4129a28a7365f57
Tobi Emmanuel <tobiemmanuel19@gmail.com>
2025-11-10 11:45:06 +0100

    modified mainApp.js

--- src/mainApp.js
+++ src/mainApp.js
@@ -18,15 +18,20 @@ app.set("views", path.join(process.cwd(), "views"));
 //handle cookie being sent to body of website
 const allowedOrigins = [
   "http://localhost:4000",
-  
-  "https://chinaemerem703.github.io/Nutrismart/"
+  "https://chinaemerem703.github.io/Nutrismart"
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser requests like Postman
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
-
 //app.use(cors({ origin: true, credentials: true }));
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
